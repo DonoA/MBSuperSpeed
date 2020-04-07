c0be106d5b177f61d1f5e64dea035977bcb96a56
Dallen <me@donovanallen.net>
2020-04-06 20:52:47 -0600

    1.1

--- MBSuperSpeed/MBSuperSpeed.cs
+++ MBSuperSpeed/MBSuperSpeed.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 using TaleWorlds.Core;
 using TaleWorlds.Engine.Screens;
 using TaleWorlds.Library;
@@ -6,11 +9,70 @@ using TaleWorlds.MountAndBlade;
 
 namespace MBSuperSpeed
 {
+    public class Settings
+    {
+        [XmlElement]
+        public string FFKey { get; set; } = "X";
 
+        [XmlElement]
+        public string FFLockKey { get; set; } = "Z";
+    }
+
+    public static class XmlUtil
+    {
+        public static T SettingsFor<T>(string moduleName)
+        {
+            string settingsPath = Path.Combine(BasePath.Name, "Modules", moduleName, "settings.xml");
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(settingsPath))
+                {
+                    XmlRootAttribute root = new XmlRootAttribute();
+                    root.ElementName = moduleName + ".Settings";
+                    root.IsNullable = true;
+
+                    if (reader.MoveToContent() != XmlNodeType.Element)
+                    {
+                        return default;
+                    }
+
+                    if (reader.Name != root.ElementName)
+                    {
+                        return default;
+                    }
+
+                    XmlSerializer serialiser = new XmlSerializer(typeof(T), root);
+                    var loaded = (T)serialiser.Deserialize(reader);
+                    return loaded;
+                }
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+    }
 
     public class SubModule : MBSubModuleBase
     {
-        private bool isHeldX = false;
+        private bool isHeldFF = false;
+
+        private TaleWorlds.InputSystem.InputKey FFKey = TaleWorlds.InputSystem.InputKey.X;
+        private TaleWorlds.InputSystem.InputKey FFLockKey = TaleWorlds.InputSystem.InputKey.Z;
+
+        protected override void OnSubModuleLoad()
+        {
+            try
+            {
+                var settings = XmlUtil.SettingsFor<Settings>("MBSuperSpeed");
+                Enum.TryParse(settings.FFKey, out FFKey);
+                Enum.TryParse(settings.FFLockKey, out FFLockKey);
+            }
+            catch(Exception ex)
+            {
+                InformationManager.DisplayMessage(new InformationMessage("Failed to load config.", Color.FromUint(4282569842U)));
+            }
+        }
 
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
@@ -26,17 +88,17 @@ namespace MBSuperSpeed
 
             ScreenBase topScreen = ScreenManager.TopScreen;
 
-            bool isTriggeredZ = topScreen != null && topScreen.DebugInput.IsControlDown() && topScreen.DebugInput.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Z);
-            bool isTriggeredX = topScreen != null && topScreen.DebugInput.IsControlDown() && topScreen.DebugInput.IsKeyDown(TaleWorlds.InputSystem.InputKey.X);
+            bool isTriggeredLockFF = topScreen != null && topScreen.DebugInput.IsControlDown() && topScreen.DebugInput.IsKeyPressed(FFLockKey);
+            bool isTriggeredFF = topScreen != null && topScreen.DebugInput.IsControlDown() && topScreen.DebugInput.IsKeyDown(FFKey);
 
-            if (isTriggeredX != isHeldX)
+            if (isTriggeredFF != isHeldFF)
             {
-                Mission.Current.SetFastForwardingFromUI(isTriggeredX);
+                Mission.Current.SetFastForwardingFromUI(isTriggeredFF);
                 InformationManager.DisplayMessage(new InformationMessage("Vroom = " + Mission.Current.IsFastForward, Color.FromUint(4282569842U)));
             }
-            isHeldX = isTriggeredX;
+            isHeldFF = isTriggeredFF;
 
-            if (isTriggeredZ)
+            if (isTriggeredLockFF)
             {
                 Mission.Current.SetFastForwardingFromUI(!Mission.Current.IsFastForward);
                 InformationManager.DisplayMessage(new InformationMessage("Vroom = " + Mission.Current.IsFastForward, Color.FromUint(4282569842U)));
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Manual Install:
+- Extract the zip file to ﻿C:\Program Files (x86)\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules.
+	- Make sure that SubModule.xml and the bin folder are now in ﻿C:\Program Files (x86)\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules\MBSuperSpeed
+- Navigate to "Modules > MBSuperSpeed > bin > Win64_Shipping_Client" in your game files.
+- Right click the "MBSuperSpeed.dll" and click properties
+- If you see an unblock at the bottom, click it. (Visual reference: https://www.limilabs.com/blog/unblock-dll-file)
+- Start the Bannerlord launcher and then tick MBSuperSpeed in the Singleplayer > Mods tab.
